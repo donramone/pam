e1464596e75816a196a23b5e5db39ea214f8e094
Luigi <luigi_lipreri@hotmail.com>
2021-10-27 13:07:44 -0300

    agregada libreria typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AreaModule } from './area/area.module';
+
 import { EmpleadosModule } from './empleados/empleados.module';
 
 @Module({
-  imports: [AreaModule, EmpleadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'luigi',
+      password: '123654789',
+      database: 'pam',
+      entities: [],
+      synchronize: true,
+    }),
+    AreaModule,
+    EmpleadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/empleados/dto/create-empleado.dto.ts
+++ src/empleados/dto/create-empleado.dto.ts
@@ -1,1 +1,16 @@
-export class CreateEmpleadoDto {}
+import { IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateEmpleadoDto {
+    @MinLength(3, { message: 'El nombre debe tener un minimo de 3 caracteres'})
+    nombre: string;
+    @IsNotEmpty({ message: 'El DNI no puede estar vacio',})
+    dni: string;
+    fechaNacimiento: string;
+    direccion: string;
+    telefono: string;
+    email: string;
+    ocupacion: string;
+    salario: number;
+    area: number;
+    
+}
